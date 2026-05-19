a486f2f348475af5d8bb5b67bc6cdc1eca80e8fb
I1HSC <sonhamne3@gmail.com>
2026-05-19 10:51:06 -0300

    Desafio Feito

---
+++ script.js
@@ -0,0 +1,10 @@
+let alunos = [
+    { nome: "Maria Gabriela", Idade: 15, materiapreferida: "Português"},
+    { nome: "Vinícius Alencar", Idade: 17, materiapreferida: "Inglês"},
+    { nome: "Lívia Bazan", Idade: 16, materiapreferida: "Geografia"},
+    { nome: "Isabella", Idade: 16, materiapreferida: "Inglês"},
+    { nome: "Miguel Arcanjo", Idade: 16, materiapreferida: "Educação Física"}
+]
+
+console.log(alunos[0].nome)
+console.log(alunos[4].materiapreferida)
